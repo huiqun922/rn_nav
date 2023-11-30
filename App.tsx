2be54551d3dc3e4201ad2cc41457add907763f67
Hay Huang <huanghuiqun@shoplazza.com>
2023-11-30 14:28:43 +0800

    refresh control

--- App.tsx
+++ App.tsx
@@ -2,40 +2,63 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, ScrollView, RefreshControl, StyleSheet, ActivityIndicator} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {useHeaderHeight} from '@react-navigation/elements';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 function HomeScreen() {
-  const headerHeight = useHeaderHeight();
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 10000);
+  }, []);
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'flex-end'}}>
-      <Text>Home Screen</Text>
-      <View
-        style={{
-          width: '100%',
-          height: headerHeight,
-          backgroundColor: 'red',
-        }}
-      />
+    <View style={{flex: 1}}>
+      <ScrollView
+        contentContainerStyle={styles.scrollView}
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }>
+        <Text>Pull down to see RefreshControl indicator</Text>
+        <ActivityIndicator size="large" />
+      </ScrollView>
     </View>
   );
 }
+function UserScreen() {
+  return <View style={{flex: 1, alignItems: 'center'}} />;
+}
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 function App() {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="User" component={UserScreen} />
+        </Tab.Navigator>
       </NavigationContainer>
     </SafeAreaProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollView: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default App;
